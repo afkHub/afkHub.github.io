b105fac3ebcb19d21d52d1723ca1fdbcb5c8a331
AFK <zipnopi@gmail.com>
2020-12-11 22:28:03 +0300

    contact me form

---
+++ contact.php
@@ -0,0 +1,29 @@
+<?php
+	$name = $_POST['name'];
+	$surname = $_POST['surname'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$email_from = 'afkwebpage@gmail.com';
+	$emai_subject = "New Contact Me Form"
+	$email_body = "User Name: $name $surname.\n".
+						"User Email: $visitor_email.\n".
+							"User Message: $message.\n";
+							
+	$to = "afk.msn@gmail.com";
+
+	$headers = "From: $email_from \r\n";
+
+	$headers .= "Reply-To: $visitor_email \r\n";
+	
+	mail($to,$email_subject,$email_body,$headers)
+	
+	header("Location: index.html");
+	
+	
+?>
+
+							
+
+
+	
